7485bf79d015ded541827f8237afcee41003ae7d
Gabriel Hazi <hazigabriel@gmail.com>
2025-04-02 20:50:05 +0300

    feat: allowed cors for FE development

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger"
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
+    app.enableCors({
+        origin: "http://localhost:3000",
+        methods: "GET,POST,PUT,DELETE",
+        allowedHeaders: "Content-Type, Authorization",
+        credentials: true,
+    })
     const config = new DocumentBuilder()
         .setTitle("Aptly")
         .setVersion("1.0")
